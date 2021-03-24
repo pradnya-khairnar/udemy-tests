e8317906b6e3ef36e32eddc9ea9af08a5a2a4e2f
Pradnya Khairnar <khairnarpradnya5@gmail.com>
2021-03-24 17:08:43 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlerComponent } from './warning-aler/warning-aler.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlerComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/warning-aler/warning-aler.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WarningAlerComponent } from './warning-aler.component';
+
+describe('WarningAlerComponent', () => {
+  let component: WarningAlerComponent;
+  let fixture: ComponentFixture<WarningAlerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WarningAlerComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WarningAlerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
